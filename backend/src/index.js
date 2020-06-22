d9b13403e13620b9c9c2a5cf4a5ee0ec881a673a
Halyson <halyson.marinho@hotmail.com>
2020-06-22 19:53:51 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+/**
+<<<<<<< HEAD:semanaomnistack11/aulas/backend/src/index.js
+ * Rota / Recurso
+=======
+ * Rota // Recurso
+>>>>>>> parent of 321d83d... Revert "Revert "Subindo back-end da aplicação"":semanaomnistack11/aulas/aulas/backend/src/index.js
+ */
+
+ /**
+  * Método HTTP:
+  * 
+  * GET: Buscar/listar uma informação do back-end
+  * POST: Criar uma informação no back-end
+  * PUT: Alterar uma informação no back-end
+  * DELETE: Deletar uma informação no back-end
+  */
+
+  /**
+   * Tipos de parâmetro:
+   * 
+   * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+   * Route Params: Parâmetros utilizados para identificar recursos
+   * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+   */
+
+   /**
+    * SQL: MySQL, SQLITE, PostgreSQL, Oracle, Microsoft SQL Server
+    * NoSQL: MongoDB, CouchDB, etc
+    */
+
+    /**
+     * Driver: SELECT * FROM users
+     * Query Builder: table('users').select('*').where()
+     */
